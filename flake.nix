3ab3cdadbdd6dc28f8f79b37d8e7c66fa908640b
Jakob Ankarhem <jakob@ankarhem.dev>
2025-10-27 21:03:46 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,115 @@
+{
+  description = "A flake with a bunch of scripts";
+
+  inputs = {
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
+    naersk.url = "github:nix-community/naersk";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    git-hooks.url = "github:cachix/git-hooks.nix";
+  };
+
+  outputs =
+    inputs:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f rec {
+            pkgs = import inputs.nixpkgs {
+              inherit system;
+              overlays = [
+                (import inputs.rust-overlay)
+                (self: super: {
+                  rustToolchain = (super.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml).override {
+                    extensions = [
+                      "rust-analyzer"
+                      "rust-src"
+                      "rustfmt"
+                    ];
+                  };
+                })
+              ];
+            };
+            naerskLib = pkgs.callPackage inputs.naersk {
+              cargo = pkgs.rustToolchain;
+              rustc = pkgs.rustToolchain;
+            };
+            pre-commit-hooks = inputs.git-hooks.lib.${system}.run {
+              src = ./.;
+              hooks = {
+                ripsecrets.enable = true;
+
+                nixfmt-rfc-style.enable = true;
+                cargo-check = {
+                  enable = false;
+                  package = pkgs.rustToolchain;
+                };
+                rustfmt = {
+                  enable = true;
+                  packageOverrides = {
+                    cargo = pkgs.rustToolchain;
+                  };
+                };
+                clippy = {
+                  enable = true;
+                  packageOverrides = {
+                    cargo = pkgs.rustToolchain;
+                    clippy = pkgs.rustToolchain;
+                  };
+                };
+              };
+            };
+          }
+        );
+    in
+    {
+      packages = forEachSupportedSystem (
+        { pkgs, naerskLib, ... }:
+        rec {
+          #          hitster = naerskLib.buildPackage {
+          #            pname = "hitster";
+          #            src = ./.;
+          #            buildInputs = with pkgs; [
+          #              openssl
+          #              sqlx-cli
+          #            ];
+          #            nativeBuildInputs = with pkgs; [ pkg-config ];
+          #          };
+        }
+      );
+
+      devShells = forEachSupportedSystem (
+        { pkgs, pre-commit-hooks, ... }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              rustToolchain
+              cargo-deny
+              cargo-edit
+              cargo-watch
+              rust-analyzer
+            ];
+
+            buildInputs = [ pre-commit-hooks.enabledPackages ];
+            shellHook = ''
+              ${pre-commit-hooks.shellHook}
+            '';
+
+            env = {
+              RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
+            };
+          };
+        }
+      );
+    };
+}
